3a3bf59d21b5dfc8341ff8ffa91b4774b3f9854b
123eeddccvvffrr44 <amar 68>
2026-01-01 08:59:40 +0530

    feat: Setup of commit lint is successful

---
+++ commitling.config.js
@@ -0,0 +1,9 @@
+module.exports ={
+    extends: ['@commitlint/config-conventional' , "@commitlint/cli"],
+    rules: {
+        'type-enum': [2, 'always', [
+            'feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'chore', 'revert'
+        ]],
+        'subject-case': [2 , 'always' , 'sentence-case']
+    }
+}
